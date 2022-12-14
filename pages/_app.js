259b34d8b2eeba5ba81c5adb066d99f9de60ef3d
William Horn <williamjosephhorn@gmail.com>
2022-12-14 15:12:37 -0500

    testing app component

--- pages/_app.js
+++ pages/_app.js
@@ -20,60 +20,46 @@ if (typeof window !== 'undefined') {
   });
 }
 
-// function MyApp({ Component, pageProps }) {
-//   const router = useRouter();
-//   const currentPage = Enum.Pages.findByMatch('url', router.pathname) || Enum.Pages._404;
-//   const lastPageRef = useRef(null);
-//   const lastPage = lastPageRef.current;
-
-//   // Update the last page visited on this website.
-//   useEffect(() => {
-//     lastPageRef.current = currentPage;
-//   });
-
-//   /* 
-//     Determine if there is a difference in layout sub-component rendering between 
-//     the current page and the last page.
-//   */
-//   const layoutDiff = (componentName) => {
-//     return lastPage && 
-//       (currentPage.excludes(componentName) !== lastPage.excludes(componentName));
-//   }
-
-//   console.log('last page: ', lastPage && lastPage.name);
-//   console.log('current page: ', currentPage.name);
-//   console.log('layout diff: ', layoutDiff('header'));
-
-//   return (
-//     <AppProvider value={{
-//       // top level state vars
-//       currentPage,
-//       lastPage,
-//       firstPageLoad: lastPage === null,
-//       layoutDiff
-//     }}>
-//       <Container add="min-h-screen bg-primary">
-//         <LayoutController>
-//           <Component {...pageProps} />
-//         </LayoutController>
-//       </Container>
-//     </AppProvider>
-//   )
-// }
-
-
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const currentPage = Enum.Pages.findByMatch('url', router.pathname) || Enum.Pages._404;
+  const lastPageRef = useRef(null);
+  const lastPage = lastPageRef.current;
+
+  // Update the last page visited on this website.
+  useEffect(() => {
+    lastPageRef.current = currentPage;
+  });
+
+  /* 
+    Determine if there is a difference in layout sub-component rendering between 
+    the current page and the last page.
+  */
+  const layoutDiff = (componentName) => {
+    return lastPage && 
+      (currentPage.excludes(componentName) !== lastPage.excludes(componentName));
+  }
+
+  console.log('last page: ', lastPage && lastPage.name);
+  console.log('current page: ', currentPage.name);
+  console.log('layout diff: ', layoutDiff('header'));
 
   return (
     <AppProvider value={{
+      // top level state vars
+      currentPage,
+      lastPage,
+      firstPageLoad: lastPage === null,
+      layoutDiff
     }}>
-      <div className="min-h-screen bg-primary">
+      <Container add="min-h-screen bg-primary">
         {/* <LayoutController> */}
           <Component {...pageProps} />
         {/* </LayoutController> */}
-      </div>
+      </Container>
     </AppProvider>
   )
 }
+
 
 export default MyApp
