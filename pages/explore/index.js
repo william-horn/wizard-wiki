d879378e1350a196c0aa6992b3c27c4b57efe24b
William Horn <williamjosephhorn@gmail.com>
2022-12-15 07:23:58 -0500

    styling explore page

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -14,25 +14,25 @@ const Explore = function() {
   // const { currentPage, lastPage } = useAppContext();
 
   return (
-    <Container add="h-full">
+    <Container>
 
-      <Container add="p-10 flex justify-evenly h-full">
+      <Container add="p-10 flex justify-evenly">
 
 
-        <Container add="w-[15%] bg-secondary rounded-[15px]">
-          <Title add="tracking-wider" className="tracking-">Menu</Title>
+        <Container add="w-[15%] bg-secondary rounded h-fit pb-3">
+          <Title add="tracking-wider shadows-font bottom-line mx-3 mb-3">Menu</Title>
           <Container add="flex flex-col">
-            <button className="p-2 mb-3 font-bold rounded bg-color-6 text-color-5">Search</button>
-            <button className="p-2 mb-3 font-bold rounded bg-color-6 text-color-5">Post</button>
-            <button className="p-2 mb-3 font-bold rounded bg-color-6 text-color-5">Start Drop Session</button>
-            <button className="p-2 mb-3 font-bold rounded bg-color-6 text-color-5">My Profile</button>
-            <button className="p-2 mb-3 font-bold rounded bg-color-6 text-color-5">Find User</button>
-            <button className="p-2 mb-3 font-bold rounded bg-color-6 text-color-5">Login</button>
+            <button className="p-2 mx-5 my-2 text-xl font-extrabold rounded-br-[20px] rounded-tl-[20px] text-white bg-[#18263d] shadow-[3px_3px_0_rgb(0,0,0)] text-shadow-md">Search</button>
+            <button className="p-2 mx-5 my-2 text-xl font-extrabold rounded-br-[20px] rounded-tl-[20px] text-white bg-[#18263d] shadow-[3px_3px_0_rgb(0,0,0)] text-shadow-md">Post</button>
+            <button className="p-2 mx-5 my-2 text-xl font-extrabold rounded-br-[20px] rounded-tl-[20px] text-white bg-[#18263d] shadow-[3px_3px_0_rgb(0,0,0)] text-shadow-md">Start Drop Session</button>
+            <button className="p-2 mx-5 my-2 text-xl font-extrabold rounded-br-[20px] rounded-tl-[20px] text-white bg-[#18263d] shadow-[3px_3px_0_rgb(0,0,0)] text-shadow-md">My Profile</button>
+            <button className="p-2 mx-5 my-2 text-xl font-extrabold rounded-br-[20px] rounded-tl-[20px] text-white bg-[#18263d] shadow-[3px_3px_0_rgb(0,0,0)] text-shadow-md">Find User</button>
+            <button className="p-2 mx-5 my-2 text-xl font-extrabold rounded-br-[20px] rounded-tl-[20px] text-white bg-[#18263d] shadow-[3px_3px_0_rgb(0,0,0)] text-shadow-md">Login</button>
           </Container>
         </Container>
 
-        <Container add="w-[75%] bg-secondary rounded-[15px]">
-          <Title add="tracking-wider" className="tracking-">Menu</Title>
+        <Container add="w-[75%] bg-secondary rounded fade-in-slow">
+          <Title add="tracking-wider text-left bottom-line mx-3 mb-7" remove="text-center">Dashboard</Title>
         </Container>
 
       </Container>
